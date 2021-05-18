90b7acb0acea6ccd1de4a741c5e6547b3f8914c8
Paul Straw <paulstraw@paulstraw.com>
2021-05-17 23:24:21 -0700

    Add BBN privacy policy page

---
+++ app/pages/bbn-privacy-policy.vue
@@ -0,0 +1,42 @@
+<docs>
+# Booster's Big Neighborhood Privacy Policy
+</docs>
+
+<template>
+  <div>
+    <h1 class="post-title">Booster's Big Neighborhood Privacy Policy</h1>
+    <p class="post-date">Posted May 17, 2021</p>
+
+    <div class="post-content">
+      <p>Thanks for playing Booster’s Big Neighborhood. This document describes how we collect, use, and handle your personal data when you use our websites, software, and services (“Services”).</p>
+
+      <hr />
+      <h2>What's Collected</h2>
+
+      <p><em>Account information.</em> We collect, and associate with your account, the information you provide to us when you do things such as create or modify your account (like your email and username).</p>
+
+      <p><em>Usage information.</em> We collect information related to how you use the Services, including actions you take in your account (like playing games, changing your username, or making purchases). We use this information to provide, improve, and promote our Services, and protect other users.</p>
+
+      <p><em>Device information.</em> We also collect information from and about the devices you use to access the Services. This includes things like IP addresses, the type of device you use, and identifiers associated with your devices. We use this information to detect abuse and identify and troubleshoot bugs.</p>
+
+      <hr>
+      <h2>What’s Done With It</h2>
+
+      <p>This information is used to provide, improve, protect, and promote our Services. Information collected may be used by third party advertising partners to show in-game ads, for example.</p>
+
+      <p>We will never contact you, or share your contact information with anyone else unless you explicitly ask us to. These are the current reasons Booster’s Big Neighborhood might send you an email:</p>
+
+      <ul>
+        <li>Password reset request for email/password sign in</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BbnPrivacyPolicy",
+
+  layout: 'blog-post',
+}
+</script>
