74b23c8cc98b734b22d11c87155a25e5c4008c7d
Paul Straw <paulstraw@paulstraw.com>
2021-05-22 17:30:08 -0700

    Add /bbn-support

---
+++ app/pages/bbn-support.vue
@@ -0,0 +1,22 @@
+<docs>
+# Booster's Big Neighborhood Support
+</docs>
+
+<template>
+  <div>
+    <h1 class="post-title">Booster's Big Neighborhood Support</h1>
+    <p class="post-date">Posted May 22, 2021</p>
+
+    <div class="post-content">
+      <p>If you have any questions, comments, or problems while playing Booster's Big Neighborhood, please contact me at <a href="mailto:paulstraw@paulstraw.com">paulstraw@paulstraw.com</a>. I don't make any guarantees about the reliability or playability of Booster's Big Neighborhood, or my response time. That said, I'll do what I can to lend a hand. :)</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BbnSupport",
+
+  layout: 'blog-post',
+}
+</script>
